8abe53f88e7ed39579bbb776148aee3614c819b0
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-01-29 09:29:55 +0100

    second commit

--- the_code.py
+++ the_code.py
@@ -1,9 +1,16 @@
-number = 0
+# number = 0
 
-while number < 7:
-    number += 3
+# while number < 7:
+#     number += 3
 
-    if number == 6:
-        continue
+#     if number == 6:
+#         continue
 
-    print(number)
+#     print(number)
+
+number = int(input("Enter a number: "))
+
+if number%2 == 0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an old number")
