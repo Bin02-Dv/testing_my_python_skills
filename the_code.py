f3332b97d98eb0bffe35ad6aa0a0d8b05a7115d2
Bin02-Dv <alaminbinahmad78@gmail.com>
2024-01-30 14:29:49 +0100

    1/30/2024

--- the_code.py
+++ the_code.py
@@ -8,9 +8,27 @@
 
 #     print(number)
 
-number = int(input("Enter a number: "))
+# number = int(input("Enter a number: "))
 
-if number%2 == 0:
-    print(f"{number} is an even number")
+# if number%2 == 0:
+#     print(f"{number} is an even number")
+# else:
+#     print(f"{number} is an old number")
+
+
+shape = input("Enter a shape: ")
+
+if shape.capitalize() == "Square":
+    print("*"*10)
+    print("*"*10)
+    print("*"*10)
+    print("*"*10)
+elif shape.capitalize() == "Triangle":
+    print(" *")
+    print(" *"*2)
+    print(" *"*3)
+    print(" *"*4)
+    print(" *"*5)
+    print(" *"*6)
 else:
-    print(f"{number} is an old number")
+    print(f"{shape} is not available")
