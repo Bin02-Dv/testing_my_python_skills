bab2576db8b941b4c003364ad87f8e33cd49dfef
Bin02-Dv <alaminbinahmad@gmail.com>
2025-04-08 15:28:42 -0700

    4/8/2025

---
+++ python_class.py
@@ -0,0 +1,26 @@
+# Variables
+
+# Swapping
+
+x = 30
+y = 54
+
+print("x is", x)
+print("y is", y)
+
+# name rules
+
+# snake casing
+# camel casing
+# pascal casing
+
+product_name = "V-Cool" # snake casing
+productName = "V-Cool" # camel casing
+ProductName = "V-Cool" # pascal casing
+
+# Data Types
+
+number = 2 # Integer
+float_number = 2.0 # Float
+string = "this is a string" # String
+boolean = True or False # Boolean
